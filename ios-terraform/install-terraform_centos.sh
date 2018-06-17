6290848456e3d9556644806dcb8baa4f83f25eae
Alex <aly@mesosphere.com>
2018-06-17 13:01:14 -0700

    Add ios-terraform

--- ios-terraform/install-terraform_centos.sh
+++ ios-terraform/install-terraform_centos.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo yum install -y zip unzip
 curl -O https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
---
+++ ios-terraform/runme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+### WIP: Set up ssh-add to be automated ###
+ssh-agent /bin/sh
+ssh-add keys/Mesosphere
+ssh-add keys/mesosphere_github
+exit
+
+### View and verify desired_cluster_profile ###
+echo
+echo
+echo Terraform Desired Profile:
+cat desired_cluster_profile
+echo
+echo
+
+read -p "Is this cluster profile correct? Input y to proceed with Terraform DC/OS Deployment (y/n) " -n1 -s c
+if [ "$c" = "y" ]; then
+
+### Grab Terraform scripts from github repo (CoreOS 1235.9.0) ###
+terraform init -from-module git@github.com:mesosphere/enterprise-terraform-dcos//aws
+terraform plan --var os=coreos_1235.9.0
+
+### Spin up DC/OS cluster with desired cluster profile ###
+terraform apply -var-file desired_cluster_profile
+
+fi
