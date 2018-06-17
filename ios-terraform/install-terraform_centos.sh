bcbefdb4b4d24f4b7fb56c1f6b3b21182fb0ba3d
Alex <aly@mesosphere.com>
2018-06-17 13:20:36 -0700

    Update

--- ios-terraform/install-terraform_centos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo yum install -y zip unzip
-curl -O https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
