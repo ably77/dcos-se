4a24688dc02fd756cd466fa2216b06b7e6648b24
Alex <aly@mesosphere.com>
2018-11-14 10:43:19 -0800

    Updates

--- Prometheus/1.11_prometheus/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#set -x #echo on
-
-dcos package uninstall prometheus --yes
-dcos package uninstall marathon-lb --yes
-dcos package uninstall grafana --yes
-dcos marathon app remove prometheus-proxy
