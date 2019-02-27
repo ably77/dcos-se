1cda16023732fe4a66c21f6035076955c2d0c6e5
Alex <aly@mesosphere.com>
2019-02-27 12:39:16 -0800

    Updates

--- RBAC/RBAC.sh
+++ RBAC/RBAC.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #set -x #echo on
 
+echo Setting Up RBAC..
+
 ### Make sure the DC/OS CLI is available
 
 result=$(dcos security 2>&1)
@@ -13,72 +15,91 @@ else
         echo Enterprise CLI has already been installed
 fi
 
-### Create frontend group and add frontend users
-dcos security org groups create frontend
-dcos security org users create -d frank -p frank frank
-dcos security org users create -d federica -p federica federica
-dcos security org groups add_user frontend frank
-dcos security org groups add_user frontend federica
+### Create prod group and add prod users
+dcos security org groups create prod
+dcos security org users create prod-user --password=deleteme -d "Production team user"
+dcos security org groups add_user prod prod-user
 
-### Create backend group and add backend users
-dcos security org groups create backend
-dcos security org users create -d bobby -p bobby bobby
-dcos security org users create -d berta -p berta berta
-dcos security org groups add_user backend bobby
-dcos security org groups add_user backend berta
+### Create dev group and add dev users
+dcos security org groups create dev
+dcos security org users create dev-user --password=deleteme -d "Development team user"
+dcos security org groups add_user dev dev-user
 
-### Create management group and add management user
-dcos security org groups create management
-dcos security org users create -d colin -p colin colin
-dcos security org groups add_user management colin
+### Create infra group and add infra users
+dcos security org groups create infra
+dcos security org users create infra-user --password=deleteme -d "Infrastructure team user"
+dcos security org groups add_user infra infra-user
+
+### Create datascience group and add datascience users
+dcos security org groups create datascience
+dcos security org users create datascience-user --password=deleteme -d "datascience team user"
+dcos security org groups add_user datascience datascience-user
 
 ### Grant group access to the native Marathon instance
-dcos security org groups grant frontend dcos:adminrouter:service:marathon full 
-dcos security org groups grant backend dcos:adminrouter:service:marathon full
-dcos security org groups grant management dcos:adminrouter:service:marathon full
+dcos security org groups grant prod dcos:adminrouter:service:marathon full
+dcos security org groups grant dev dcos:adminrouter:service:marathon full
+dcos security org groups grant infra dcos:adminrouter:service:marathon full
+dcos security org groups grant datascience dcos:adminrouter:service:marathon full
 
 ### Grant group access to launch DC/OS Services
-# Frontend and Backend groups only have access to launch services in their respective team group folder (e.g. /frontend/nginx)
-# Management team is able to manage both frontend, backend, and root folders
-dcos security org groups grant frontend dcos:service:marathon:marathon:services:frontend full
-dcos security org groups grant backend dcos:service:marathon:marathon:services:backend full
-dcos security org groups grant management dcos:service:marathon:marathon:services full
+# prod and dev groups only have access to launch services in their respective team group folder (e.g. /prod/nginx)
+dcos security org groups grant prod dcos:service:marathon:marathon:services:prod full
+dcos security org groups grant dev dcos:service:marathon:marathon:services:dev full
+dcos security org groups grant infra dcos:service:marathon:marathon:services:infra full
+dcos security org groups grant datascience dcos:service:marathon:marathon:services:datascience full
 
-### Grant access to view the Jobs tab in the UI (Note: Only Backend and Management Teams to show differences in views)
-dcos security org groups grant backend dcos:adminrouter:service:metronome full
-dcos security org groups grant management dcos:adminrouter:service:metronome full
+### Grant access to view the Jobs tab in the UI
+dcos security org groups grant dev dcos:adminrouter:service:metronome full
+dcos security org groups grant prod dcos:adminrouter:service:metronome full
+dcos security org groups grant infra dcos:adminrouter:service:metronome full
+dcos security org groups grant datascience dcos:adminrouter:service:metronome full
 
 ### Grant permission to launch packages from the DC/OS Universe
-# Groups only have access to launch services in their respective team group folder (e.g. /frontend/cassandra)
-dcos security org groups grant frontend dcos:adminrouter:package full
-dcos security org groups grant backend dcos:adminrouter:package full
-dcos security org groups grant management dcos:adminrouter:package full
+# Groups only have access to launch services in their respective team group folder (e.g. /prod/cassandra)
+dcos security org groups grant prod dcos:adminrouter:package full
+dcos security org groups grant dev dcos:adminrouter:package full
+dcos security org groups grant infra dcos:adminrouter:package full
+dcos security org groups grant datascience dcos:adminrouter:package full
 
 ### Grant permission to access the Mesos master UI and API
-dcos security org groups grant frontend dcos:adminrouter:ops:mesos full
-dcos security org groups grant backend dcos:adminrouter:ops:mesos full
-dcos security org groups grant management dcos:adminrouter:ops:mesos full
+dcos security org groups grant prod dcos:adminrouter:ops:mesos full
+dcos security org groups grant dev dcos:adminrouter:ops:mesos full
+dcos security org groups grant infra dcos:adminrouter:ops:mesos full
+dcos security org groups grant datascience dcos:adminrouter:ops:mesos full
 
 ### Grant group access to Mesos Task details and logs
-dcos security org groups grant frontend dcos:adminrouter:ops:slave full
-dcos security org groups grant backend dcos:adminrouter:ops:slave full
-dcos security org groups grant management dcos:adminrouter:ops:slave full
-
-### Grant group access to Security and Access Management features in the UI
-# Manager group only
-dcos security org groups grant management dcos:adminrouter:acs full
-
-### Grant group access to the DC/OS Components tab to view system health
-# Manager group only
-dcos security org groups grant management dcos:adminrouter:ops:system-health full
+dcos security org groups grant prod dcos:adminrouter:ops:slave full
+dcos security org groups grant dev dcos:adminrouter:ops:slave full
+dcos security org groups grant infra dcos:adminrouter:ops:slave full
+dcos security org groups grant datascience dcos:adminrouter:ops:slave full
 
 ### Grant group access to the Networking tab in the UI
-# Frontend and Management team only
-dcos security org groups grant frontend dcos:adminrouter:ops:networking full
-dcos security org groups grant management dcos:adminrouter:ops:networking full
+dcos security org groups grant prod dcos:adminrouter:ops:networking full
 
+### Grant group access to the /prod secrets
+dcos security secrets create /prod/example-secret --value="prod-team-secret"
+dcos security org groups grant prod dcos:secrets:list:default:/prod full
+dcos security org groups grant prod dcos:secrets:default:/prod/* full
+# Appears to be necessary per COPS-2534
+dcos security org groups grant prod dcos:secrets:list:default:/ read
 
+### Grant group access to the /dev secrets
+dcos security secrets create /dev/example-secret --value="dev-team-secret"
+dcos security org groups grant dev dcos:secrets:list:default:/dev full
+dcos security org groups grant dev dcos:secrets:default:/dev/* full
+# Appears to be necessary per COPS-2534
+dcos security org groups grant dev dcos:secrets:list:default:/ read
 
+### Grant group access to the /infra secrets
+dcos security secrets create /infra/example-secret --value="infra-team-secret"
+dcos security org groups grant infra dcos:secrets:list:default:/infra full
+dcos security org groups grant infra dcos:secrets:default:/infra/* full
+# Appears to be necessary per COPS-2534
+dcos security org groups grant infra dcos:secrets:list:default:/ read
 
-
-
+### Grant group access to the /datascience secrets
+dcos security secrets create /datascience/example-secret --value="datascience-team-secret"
+dcos security org groups grant datascience dcos:secrets:list:default:/datascience full
+dcos security org groups grant datascience dcos:secrets:default:/datascience/* full
+# Appears to be necessary per COPS-2534
+dcos security org groups grant datascience dcos:secrets:list:default:/ read
--- RBAC/README.md
+++ RBAC/README.md
@@ -6,32 +6,17 @@
 
 ### Instructions:
 Run the RBAC.sh script
-	
-This script will create three groups and associated users: Frontend, Backend, and Management and will assign specific permissions to each group
-	 
-### Permissions Description:
-Frontend: (Users: frank, federica)
-- Frontend team has permission to view/deploy Marathon and Data Services into the /frontend folder
-- Frontend team has permission to view the Networking tab, but Backend team does not (just to show differentiation and access control)
 
-Backend Group: (Users: bobby, berta)
-- Backend team has permission to view/deploy Marathon and Data Services into the /backend folder
-- Backend team has permission to view/deploy Metronome Jobs but Frontend team does not (just to show differentiation and access control)
+This script will create four groups and associated users: Prod, Dev, Datascience, and Infra and will assign specific permissions to each group
 
-Management Group: (Users: colin)
-- Management has permission to view/deploy Marathon and Data Services into both /frontend and /backend folders as well as the root folder
-- Management team has permission to view/deploy Metronome Jobs
-- Access to the RBAC tab view and full access to add/remove permissions
-- Access to the Networking tab
-- Access to the Components tab in the UI to view system health
-
+### Users:
+prod-user // deleteme
+dev-user // deleteme
+datascience-user // deleteme
+infra-user //deleteme
 
 ### Demo Workflow:
 1. Run script `./RBAC.sh`
 2. Show Superuser full view
-3. Login to Frontend/Backend personas and test deploy into root Marathon folder and watch it fail. Retry the deployment into the team folder and watch it work
-4. Login to the Backend/Management personas and show the Jobs tab and run a sample job
-5. Login to Management persona to see differences set up between the Superuser and Manager
-6. In the management persona, deploy to either frontend or backend folder or remove other user's existing instances
-7. In the management persona, show networking and components tab access
-8. Show that the manager can add/remove permissions through the GUI
+3. Show how a superuser sets up groups, users, and permissions
+3. Show prod/dev/datascience/infra users views and that each user can only deploy into their own team folders
